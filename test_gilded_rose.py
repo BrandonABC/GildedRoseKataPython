5dd51e984900e72d3562c182400667b1af583d0d
Brandon Yu <brandonyu@BY-MacBook-Pro.local>
2025-02-05 21:12:39 -0800

    lab4

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -5,11 +5,50 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    # example of test that checks for logical errors
+    def test_sulfuras_should_not_decrease_quality(self):
+        items = [Item("Sulfuras", 5, 80)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        sulfuras_item = items[0]
+        self.assertEqual(80, sulfuras_item.quality)
+        self.assertEqual(4, sulfuras_item.sell_in)
+        self.assertEqual("Sulfuras", sulfuras_item.name)
+        print("run1")
+    
+    # example of test that checks for syntax errors
+    def test_gilded_rose_list_all_items(self):
+        items = [Item("Sulfuras", 5, 80)]
+        gilded_rose = GildedRose(items)
+        all_items = gilded_rose.get_items()
+        self.assertEqual(["Sulfuras"], all_items)
+        print("run2")
+    
+    def test_quality_decreases_twice_as_fast_after_sell_by(self):
+        items = [Item("Regular Item", 0, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(6, items[0].quality)  # Expect it to decrease by 4 (more strict)
+
+    def test_aged_brie_increases_quality_twice_after_sell_by(self):
+        items = [Item("Aged Brie", 0, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(14, items[0].quality)  # Expect it to increase by 4 (more strict)
+
+    def test_backstage_passes_quality_increases(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 10, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(24, items[0].quality)  # Expect it to increase by 4 (more strict)
+
+    def test_conjured_items_degrade_twice_as_fast(self):
+        items = [Item("Conjured Mana Cake", 3, 6)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(2, items[0].quality)  # Expect it to decrease by 4 (more strict)
+
+
 
 
 if __name__ == '__main__':
